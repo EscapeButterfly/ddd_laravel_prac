6925bf34c05879f169a4ff539b642183f9996ee9
Vitalii Horovoi <vitalii.gorovoii@gmail.com>
2023-11-08 21:01:48 +0100

    Added form request for author update.

---
+++ src/app/Store/Catalog/Presentation/HTTP/Requests/UpdateAuthorRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Store\Catalog\Presentation\HTTP\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateAuthorRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name'  => 'required',
+            'second_name' => 'required',
+            'birth_date'  => 'required|date',
+            'biography'   => 'required'
+        ];
+    }
+}
